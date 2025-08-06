257679f1784e7e3a733d4dc01c5c50b8c5539266
elioskaram <74376068+elioskaram@users.noreply.github.com>
2025-08-06 15:47:30 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "tenant_id" { 
+ type = string 
+ default = "60a5e6b0-6783-462c-a4a4-08c0cd9c5706" 
+} 
+variable "subscription_id" { 
+ type = string 
+ default = "4dbffbb6-92ea-4699-bba4-5c52b58301ff" #modify accordingly
+} 
+variable "rg_name" { 
+ type = string 
+ default = "sandbox_farah.kacem" #modify accordingly
+} 
+variable "location" { 
+ type  = string 
+ default = "West Europe"
+} 
